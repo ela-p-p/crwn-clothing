9b99d01911a3ae64abb06341cdc7942c2900f9b8
ela-p-p <elizabeth.porter.pevec@gmail.com>
2020-01-29 17:16:37 -0500

    implemented reselect library for cart components

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -3,21 +3,24 @@ import { connect } from "react-redux";
 import CartItem from "../CartItem/CartItem";
 import CustomButton from "../CustomButton/CustomButton";
 
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 import "./cartDropdown.scss";
 
-const CartDropdown = ({ cartItems }) => (
-  <div className="cart-dropdown">
-    <div className="cart-items">
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
-    </div>
-
-    <CustomButton>go to checkout</CustomButton>
+const CartDropdown = ({ cartItems }) => {
+  console.log('art', cartItems)
+  return (<div className="cart-dropdown">
+  <div className="cart-items">
+    {cartItems.map(cartItem => (
+      <CartItem key={cartItem.id} item={cartItem} />
+    ))}
   </div>
-);
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+  <CustomButton>go to checkout</CustomButton>
+</div>)
+  
+};
+
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 export default connect(mapStateToProps)(CartDropdown);
